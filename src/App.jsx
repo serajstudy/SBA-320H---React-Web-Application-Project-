6f5fbc878a9ecc805dd02722ad1ead1250ede74c
seraj <serajstudy1@gmail.com>
2025-10-08 03:11:23 -0400

    commit app

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import About from './pages/About';
-import './App.css'
+
+import './App.css';
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="app">
+        <header>
+          <h1>Pokemon-themed website</h1>
+          <nav>
+            <Link to="/">Home</Link> <br />
+            <Link to="/products">Products</Link> <br />
+            <Link to="/about">About</Link> <br />
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
